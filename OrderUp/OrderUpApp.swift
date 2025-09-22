1a2a8acb0fd14ef8249565d9ed556a92fc923660
Mac <macjacobson99@gmail.com>
2025-09-22 13:48:58 -0600

    Initial Commit

---
+++ OrderUp/OrderUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderUpApp.swift
+//  OrderUp
+//
+//  Created by Jacobson, Machiah - Student on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct OrderUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OrderUpTests/OrderUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrderUpTests.swift
+//  OrderUpTests
+//
+//  Created by Jacobson, Machiah - Student on 9/22/25.
+//
+
+import Testing
+@testable import OrderUp
+
+struct OrderUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
